096a48e018203fbc77bad7d292e8bf205651f6f8
ashutoshumar <viraj28300@gmail.com>
2022-01-17 14:41:52 +0530

    hopeee

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import HomeScreen from "./HomeScreen";
 import Upload from "./Components/Upload";
-import { Route,Routes , Navigate,Link} from "react-router-dom"
+import { Route, Routes, Navigate,Link, Redirect, Switch} from "react-router-dom"
 function App() {
   return (
     <>
-     <Routes >
-            <Route exact path="/">
-               <HomeScreen/>            
-           </Route>
-           <Route exact path="/about">
+   
+      <Switch>
+      <Route exact path="/" ><HomeScreen/></Route>
+            <Route exact path="/about">
              About
            </Route>
            <Route exact path="/contact">
@@ -17,15 +16,13 @@ function App() {
            </Route>
            <Route exact path="/services">Services
               
-           </Route>
-           <Route exact path="/Upload">
-              <Upload/>
-           </Route>
-           
+           </Route> 
+           <Route path="/upload"><Upload/></Route>
            <Route>
-               < Navigate to="/"/>
+             <Redirect to="/"/>
            </Route>
-            </Routes >
+          
+            </Switch> 
     </>
   );
 }
