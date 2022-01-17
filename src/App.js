55b4fccedd8ee3b158b85c3607035549cca02985
ashutoshumar <viraj28300@gmail.com>
2022-01-17 13:54:37 +0530

    hopee

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import HomeScreen from "./HomeScreen";
 import Upload from "./Components/Upload";
-import { Route,Switch, Navigate,Link} from "react-router-dom"
+import { Route,Routes , Navigate,Link} from "react-router-dom"
 function App() {
   return (
     <>
-     <Switch>
+     <Routes >
             <Route exact path="/">
                <HomeScreen/>            
            </Route>
@@ -25,7 +25,7 @@ function App() {
            <Route>
                < Navigate to="/"/>
            </Route>
-            </Switch>
+            </Routes >
     </>
   );
 }
